2b1204345a72d2104536391cb124c9fdd2ee79e4
tony-728 <ksang0728@gmail.com>
2022-11-21 06:14:07 +0000

    update train.py, functional separation

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import torch
 from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification
 
+from sklearn.model_selection import KFold
+
 from tqdm import tqdm
 import wandb
 
@@ -8,39 +10,60 @@ from Metric import compute_loss, compute_metrics
 from load_data import RE_Dataset
 import utils
 
-from typing import Dict
+from typing import Dict, Optional
 import warnings
 
 warnings.filterwarnings(action="ignore")
 
 
-def train(config: Dict) -> str:
+def set_wandb(config: Dict, model, project: str):
     """
-    입력된 config에 따라서 모델 학습을 진행한다.
-    학습 동안 가장 낮은 loss를 기록한 모델을 저장한다.
+    wandb 관련 세팅
 
     Parameters
     ----------
     config : Dict
-        config Dictionary
-        "wandb":
-            wandb logging check: true/false,
-        "wandb_key":
-            "<YOUR wandb API KEY>",
-        "inference":
-            학습완료 후 inference 진행 check: true/false,
-        "train_data_path": "<train data path>",
-        "val_data_path": "<val data path>",
-        "test_data_path": "<test data path>",
-        "model_name": "<pre-trained model name>",
-        "epoch": number of epoch,
-        "batch_size": number of batch_size,
-        "lr": learning rate
+        모델학습과 관련된 config
+    model : _type_
+        학습에 사용될 모델
+    project : str
+        wandb project name
+    """
+    wandb.login(key=config["wandb_key"])
+    entity = "nlp02"
+    wandb.init(
+        entity=entity,
+        project=project,
+        name=f"(batch:{config['batch_size']},epoch:{config['epoch']},lr:{config['lr']})",
+    )
+    wandb.watch(model, log_freq=100)
+    return
+
+
+def set_train(config: Dict):
+    """
+    모델 학습에 필요한 것들을 생성한다.
+    K-fold로 검증할 때와 아닐 때를 구분한다.
+
+    K-fold가 아닐 때
+    - 모델
+    - train dataloader
+    - val dataloader
+    - optimizer
+
+    K-fold 일 때
+    - K-fold 객체
+    - total dataset
+
+    Parameters
+    ----------
+    config : Dict
+        모델학습과 관련된 config
 
     Returns
     -------
-    str
-        저장된 모델 경로
+    Optional
+        _description_
     """
     tokenizer = AutoTokenizer.from_pretrained(config["model_name"])
 
@@ -48,6 +71,19 @@ def train(config: Dict) -> str:
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset, batch_size=config["batch_size"], shuffle=False
     )
+
+    if config["k-fold"]:
+        kfold_config = config["k-fold_config"]
+        if kfold_config["shuffle"]:
+            kf = KFold(
+                n_splits=kfold_config["num_splits"],
+                shuffle=True,
+                random_state=kfold_config["split_seed"],
+            )
+        else:
+            kf = KFold(n_splits=kfold_config["num_splits"], shuffle=False)
+
+        return kf, train_dataset
 
     val_dataset = RE_Dataset(config["val_data_path"], tokenizer)
     val_dataloader = torch.utils.data.DataLoader(
@@ -62,22 +98,41 @@ def train(config: Dict) -> str:
 
     optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"])
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    return model, train_dataloader, val_dataloader, optimizer
 
+
+def training(config: Dict, model, train_dataloader, val_dataloader, optimizer):
+    """
+    실제 학습을 진행한다.
+
+    Parameters
+    ----------
+    config : _type_
+        모델학습과 관련된 config
+    model : _type_
+        학습에 사용할 모델
+    train_dataloader : _type_
+        학습에 사용할 dataloader
+    val_dataloader : _type_
+        검증에 사용할 dataloader
+    optimizer : _type_
+        최적화 함수
+
+    Returns
+    -------
+    K-fold로 진행하지 않을 때
+        loss가 가장 낮은 모델의 저장 경로
+    K-fold로 진행할 때
+        최종 validation loss
+    """
     # wandb setting
     project = config["model_name"].replace("/", "-")
 
     if config["wandb"]:
-        wandb.login(key=config["wandb_key"])
-        entity = "nlp02"
-        wandb.init(
-            entity=entity,
-            project=project,
-            name=f"(batch:{config['batch_size']},epoch:{config['epoch']},lr:{config['lr']})",
-        )
-        wandb.watch(model, log_freq=100)
+        set_wandb(config, model, project)
 
-    ##training
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     lowest_valid_loss = 9999.0
 
     model.to(device)
@@ -132,28 +187,126 @@ def train(config: Dict) -> str:
         print(f"epoch: {epoch_num} val loss: {val_loss}")
 
         if config["wandb"]:
-            wandb.log({"eval_loss": float(sum(epoch_loss) / len(epoch_loss))})
+            wandb.log({"eval_loss": val_loss})
             wandb.log({"eval_f1": metrics["micro f1 score"]})
             wandb.log({"eval_auprc": metrics["auprc"]})
             wandb.log({"eval_accuracy": metrics["accuracy"]})
 
-        if lowest_valid_loss > val_loss:
-            save_model_dir = f"./best_model/{project}"
-            if utils.create_directory(save_model_dir):
-                save_model_path = f"{save_model_dir}/{project}_b{config['batch_size']}_e{config['epoch']}_lr{config['lr']}.bin"
-                print(
-                    "Acc for model which have lower valid loss: ", metrics["accuracy"]
-                )
-                torch.save(
-                    model.state_dict(),
-                    save_model_path,
-                )
-                lowest_valid_loss = val_loss
+        if not config["k-fold"]:
+            if lowest_valid_loss > val_loss:
+                save_model_dir = f"./best_model/{project}"
+                if utils.create_directory(save_model_dir):
+                    save_model_path = f"{save_model_dir}/{project}_b{config['batch_size']}_e{config['epoch']}_lr{config['lr']}.bin"
+                    print(
+                        "Acc for model which have lower valid loss: ",
+                        metrics["accuracy"],
+                    )
+                    torch.save(
+                        model.state_dict(),
+                        save_model_path,
+                    )
+                    lowest_valid_loss = val_loss
+
+    if config["k-fold"]:
+        # 마지막 validation loss, metrics 리턴
+        return val_loss, metrics
 
     print("---- Train Finish! ----")
     return save_model_path
 
-from sklearn.model_selection import KFold
+
+def train(config: Dict) -> Optional[str]:
+    """
+    입력된 config에 따라서 모델 학습을 진행한다.
+    학습 동안 가장 낮은 loss를 기록한 모델을 저장한다.
+
+    Parameters
+    ----------
+    config : Dict
+        config Dictionary
+        "wandb":
+            wandb logging check: true/false,
+        "wandb_key":
+            "<YOUR wandb API KEY>",
+        "inference":
+            학습완료 후 inference 진행 check: true/false,
+        "train_data_path": "<train data path>",
+        "val_data_path": "<val data path>",
+        "test_data_path": "<test data path>",
+        "model_name": "<pre-trained model name>",
+        "epoch": number of epoch,
+        "batch_size": number of batch_size,
+        "lr": learning rate,
+        "k-fold":
+            k-fold 적용 여부 check: true/false,
+        "k-fold_config":{
+            "num_splits": number of folds,
+            "split_seed": random seed value,
+            "shuffle":
+                k-fold split suffle 여부 check: true/false,
+        }
+
+    Returns
+    -------
+    Optional[str]
+        저장된 모델 경로
+    """
+    if config["k-fold"]:
+        kf, total_dataset = set_train(config)
+
+        total_loss = []  # 각 폴드별로 나온 로스 넣어서 최종적으로 평균 떄려서 출력
+
+        for fold, (train_idx, val_idx) in enumerate(kf.split(total_dataset)):
+            print("------------fold no---------{}----------------------".format(fold))
+            train_subsampler = torch.utils.data.SubsetRandomSampler(train_idx)
+            val_subsampler = torch.utils.data.SubsetRandomSampler(val_idx)
+
+            train_dataloader = torch.utils.data.DataLoader(
+                total_dataset,
+                batch_size=config["batch_size"],
+                shuffle=False,
+                sampler=train_subsampler,
+            )
+
+            val_dataloader = torch.utils.data.DataLoader(
+                total_dataset,
+                batch_size=config["batch_size"],
+                shuffle=False,
+                sampler=val_subsampler,
+            )
+
+            model_config = AutoConfig.from_pretrained(config["model_name"])
+            model_config.num_labels = 30
+
+            model = AutoModelForSequenceClassification.from_pretrained(
+                config["model_name"], config=model_config
+            )
+
+            optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"])
+
+            val_loss, total_metrics = training(
+                config, model, train_dataloader, val_dataloader, optimizer
+            )
+
+            # k-fold로 검증하는 것은 각 fold의 loss, metrics를 구하고
+            # 평균을 내서 최종적으로 loss와 metrics을 구한다.
+            total_loss.append(val_loss)
+
+            # 혹시 metrics도 추가하신다면 추가해주세요
+            ######################################
+
+        print(f"K-fold mean loss: {sum(total_loss)/len(total_loss)}")
+        return None
+    else:
+        model, train_dataloader, val_dataloader, optimizer = set_train(config)
+
+        ##training
+        save_model_path = training(
+            config, model, train_dataloader, val_dataloader, optimizer
+        )
+
+        return save_model_path
+
 
 def trainKFold(config: dict) -> str:
     """
@@ -193,32 +346,36 @@ def trainKFold(config: dict) -> str:
     """
     tokenizer = AutoTokenizer.from_pretrained(config["model_name"])
 
-    kfold_config=config["k-fold_config"]
+    kfold_config = config["k-fold_config"]
     total_dataset = RE_Dataset(config["train_data_path"], tokenizer)
 
     # 데이터셋 num_splits 번 fold
     if kfold_config["shuffle"]:
-        kf = KFold(n_splits=kfold_config["num_splits"], shuffle=True, random_state=kfold_config["split_seed"])
+        kf = KFold(
+            n_splits=kfold_config["num_splits"],
+            shuffle=True,
+            random_state=kfold_config["split_seed"],
+        )
     else:
         kf = KFold(n_splits=kfold_config["num_splits"], shuffle=False)
 
-    for fold,(train_idx,val_idx) in enumerate(kf.split(total_dataset)):
-        print('------------fold no---------{}----------------------'.format(fold))
+    for fold, (train_idx, val_idx) in enumerate(kf.split(total_dataset)):
+        print("------------fold no---------{}----------------------".format(fold))
         train_subsampler = torch.utils.data.SubsetRandomSampler(train_idx)
         val_subsampler = torch.utils.data.SubsetRandomSampler(val_idx)
 
         train_dataloader = torch.utils.data.DataLoader(
-            total_dataset, 
-            batch_size=config["batch_size"], 
+            total_dataset,
+            batch_size=config["batch_size"],
             shuffle=False,
-            sampler=train_subsampler
+            sampler=train_subsampler,
         )
 
         val_dataloader = torch.utils.data.DataLoader(
-            total_dataset, 
-            batch_size=config["batch_size"], 
+            total_dataset,
+            batch_size=config["batch_size"],
             shuffle=False,
-            sampler=val_subsampler
+            sampler=val_subsampler,
         )
 
         model_config = AutoConfig.from_pretrained(config["model_name"])
@@ -230,92 +387,7 @@ def trainKFold(config: dict) -> str:
 
         optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"])
 
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-        # wandb setting
-        project = config["model_name"].replace("/", "-")
-
-        if config["wandb"]:
-            wandb.login(key=config["wandb_key"])
-            entity = "nlp02"
-            wandb.init(
-                entity=entity,
-                project=project,
-                name=f"(fold:{fold},batch:{config['batch_size']},epoch:{config['epoch']},lr:{config['lr']})",
-            )
-            wandb.watch(model, log_freq=100)
-
-        ##training
-        lowest_valid_loss = 9999.0
-
-        model.to(device)
-
-        for epoch_num in range(config["epoch"]):
-            model.train()
-            epoch_loss = []
-            with tqdm(train_dataloader, unit="batch") as tepoch:
-                for i, (item, labels) in enumerate(tepoch):
-                    tepoch.set_description(f"Epoch {epoch_num}")
-
-                    optimizer.zero_grad()
-
-                    batch = {k: v.to(device) for k, v in item.items()}
-                    pred = model(**batch).logits
-                    loss = compute_loss(pred, labels.to(device))
-                    epoch_loss.append(loss)
-
-                    loss.backward()
-                    optimizer.step()
-
-                    tepoch.set_postfix(loss=loss.item())
-
-                    if config["wandb"]:
-                        wandb.log({"train_loss": loss.item()})
-
-                print(
-                    f"epoch: {epoch_num} train loss: {float(sum(epoch_loss) / len(epoch_loss))}"
-                )
-
-            val_loss = []
-            val_pred = []
-            val_labels = []
-            model.eval()
-            with torch.no_grad():
-                for i, (item, labels) in enumerate(tqdm(val_dataloader, desc="Eval")):
-                    batch = {k: v.to(device) for k, v in item.items()}
-                    pred = model(**batch).logits
-                    val_pred.append(pred)
-                    val_labels.append(labels)
-
-                    loss = compute_loss(pred, labels.to(device))
-                    val_loss.append(loss)
-
-            val_pred = torch.cat(val_pred, dim=0).detach().cpu().numpy()
-            val_labels = torch.cat(val_labels, dim=0).detach().cpu().numpy()
-
-            metrics = compute_metrics(val_pred, val_labels)
-            print(metrics)
-
-            val_loss = float(sum(val_loss) / len(val_loss))
-            print(f"epoch: {epoch_num} val loss: {val_loss}")
-
-            if config["wandb"]:
-                wandb.log({"eval_loss": float(sum(epoch_loss) / len(epoch_loss))})
-                wandb.log({"eval_f1": metrics["micro f1 score"]})
-                wandb.log({"eval_auprc": metrics["auprc"]})
-                wandb.log({"eval_accuracy": metrics["accuracy"]})
-
-            if lowest_valid_loss > val_loss:
-                save_model_path = f"best_model/{project}/{fold}/{project}_b{config['batch_size']}_e{config['epoch']}_lr{config['lr']}.bin"
-                print("Acc for model which have lower valid loss: ", metrics["accuracy"])
-                torch.save(
-                    model.state_dict(),
-                    save_model_path,
-                )
-                lowest_valid_loss = val_loss
-
-    print("---- Train Finish! ----")
-    return save_model_path
+        training(config, model, train_dataloader, val_dataloader, optimizer)
 
 
 if __name__ == "__main__":
