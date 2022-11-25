5a2f7f751cffbf78348dfb2718ac7349a18e18db
tony-728 <ksang0728@gmail.com>
2022-11-25 04:58:01 +0000

    modify: model.py classifier, entity embedding

--- Model.py
+++ Model.py
@@ -9,32 +9,45 @@ class Model(nn.Module):
     def __init__(self, model_name: str):
         super().__init__()
         self.model = AutoModel.from_pretrained(model_name)
-        hidden_state = self.model.config.hidden_size
-        self.linear = nn.Linear(hidden_state*2, 30)
-        self.dense = nn.Linear(hidden_state*2,hidden_state*2)
-        self.tanh = nn.Tanh()
-        self.dropout = nn.Dropout(0.1)
+        hidden_size = self.model.config.hidden_size
+
+        self.classifier = nn.Sequential(
+            nn.Linear(hidden_size * 2, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, 30),
+        )
 
     def forward(self, batch, markers):
-        output = self.model(**batch)
-        last_hidden = output.last_hidden_state
-        batch_output_list = []
-        for _ in range(last_hidden.size(0)):
-            ss = markers['ss'][_]
-            se = markers['se'][_]
-            os = markers['os'][_]
-            oe = markers['oe'][_]
-            subj_output = torch.mean(last_hidden[_][ss:se+1], dim = 0) #(768)
-            obj_output = torch.mean(last_hidden[_][os:oe+1], dim = 0) #(768)
+        outputs = self.model(**batch)
 
-            batch_output_list.append(torch.cat((subj_output, obj_output), dim = 0)) #(768*3)
+        # subject, object의 첫번째 embedding
+        pooled_output = (
+            outputs.last_hidden_state
+        )  # 0번 last_hidden_state, 1번 pooler_output
 
-        batch_output = torch.stack(batch_output_list)
-        batch_output = self.dropout(batch_output)
-        batch_output = self.dense(batch_output)
-        batch_output = self.tanh(batch_output)
-        batch_output = self.dropout(batch_output)
-        batch_output = self.linear(batch_output)
+        idx = torch.arange(batch["input_ids"].size(0)).to(batch["input_ids"].device)
+        ss_emb = pooled_output[idx, markers["ss"]]
+        os_emb = pooled_output[idx, markers["os"]]
+        h = torch.cat((ss_emb, os_emb), dim=-1)
+        logits = self.classifier(h)
 
-        return batch_output
-        
+        # subject, object embedding의 평균
+        # last_hidden = outputs.last_hidden_state
+        # batch_output_list = []
+        # for _ in range(last_hidden.size(0)):
+        #     ss = markers["ss"][_]
+        #     se = markers["se"][_]
+        #     os = markers["os"][_]
+        #     oe = markers["oe"][_]
+        #     subj_output = torch.mean(last_hidden[_][ss : se + 1], dim=0)  # (768)
+        #     obj_output = torch.mean(last_hidden[_][os : oe + 1], dim=0)  # (768)
+
+        #     batch_output_list.append(
+        #         torch.cat((subj_output, obj_output), dim=0)
+        #     )  # (768*3)
+
+        # batch_output = torch.stack(batch_output_list)
+        # batch_output = self.classifier(batch_output)
+
+        return logits
